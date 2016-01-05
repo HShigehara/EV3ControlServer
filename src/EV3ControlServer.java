41954a7641f0959488cd8495ccb9320900ea72c5
HShigehara <chilly_0427@hotmail.co.jp>
2016-01-05 17:31:01 +0900

    add Timer Task

--- src/EV3ControlServer.java
+++ src/EV3ControlServer.java
@@ -1,4 +1,31 @@
+/*
+  TimerTaskを二つ動作させる例
+  以下のようにすると、t1, t2 が並列で動作する
+  
+  schedule は割り込み間隔が次第に遅延していく
+  scheduleAtFixedRate は当初の割り込み間隔を守ろうとする
+  当然後者を利用しましょう。
+*/
+import java.util.Timer;
 
-public class EV3ControlServer {
+public class EV3ControlServer{
+    public static void main(String[] args){
+	Timer t1 = new Timer();
+	Timer t2 = new Timer();
 
+	t1.schedule(new TimeSchedulingServer("First"), 1000, 100);
+	t2.scheduleAtFixedRate(new TimeSchedulingServer("Second"), 1000, 100);
+
+	// 5000 [msec]だけ動作
+	try{
+	    Thread.sleep(5000);
+	} catch(InterruptedException e){
+	    System.out.println("error : " + e);
+	}
+
+	// タスクの停止
+	t1.cancel();
+	t2.cancel();
+
+    }
 }
--- src/TimeSchedulingServer.java
+++ src/TimeSchedulingServer.java
@@ -1,4 +1,18 @@
+import java.util.TimerTask;
+import java.util.concurrent.TimeUnit;
 
-public class TimeSchedulingServer {
+public class TimeSchedulingServer extends TimerTask{
+    private long timeStart;
+    private String id;
 
+    TimeSchedulingServer(String arg){
+	timeStart = System.nanoTime();
+	id = arg;
+    }
+
+    public void run(){
+	long timeNow = System.nanoTime() - timeStart;
+
+	System.out.println(id +  " : " + TimeUnit.MILLISECONDS.convert(timeNow, TimeUnit.NANOSECONDS));
+    }
 }
